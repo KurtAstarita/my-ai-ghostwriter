3ba5bdd4c4b8e32b04cdd526510075d5bb2d94dc
KurtAstarita <itskurtastarita@gmail.com>
2025-04-15 11:00:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY') or 'your_fallback_secret_key'
-CORS(app)
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://kurtastarita.github.io"}})
 csrf = CSRFProtect(app) # Initialize CSRF protection
 limiter = Limiter(get_remote_address, app=app, storage_uri="memory://")
 
@@ -21,7 +21,6 @@ allowed_attributes = {}
 
 @app.route('/csrf_token', methods=['GET'])
 def get_csrf_token():
-    # We might just need to trigger the CSRF protection to set the cookie
     return jsonify({'success': True})
 
 @app.route('/')
