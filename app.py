aaaa849fced8b13541a5cd6d0edec5e50ed71151
KurtAstarita <itskurtastarita@gmail.com>
2025-04-16 04:00:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY') or 'your_fallback_secret_key'
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://kurtastarita.github.io"}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://myaighostwriter.kurtastarita.com"}})
 # csrf = CSRFProtect(app) # Comment out initialization
 limiter = Limiter(get_remote_address, app=app, storage_uri="memory://")
 
